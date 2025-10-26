446378e169bb9d2f3c5821869ab0fe270fdf235e
Eduardo Rodrigues <edusacramento9@gmail.com>
2025-10-26 20:53:55 -0300

    feat: Adicionar repositórios de categorias e produtos
    Implementa as interfaces para repositórios de categorias e produtos, incluindo métodos para criação, busca, atualização, listagem e deleção, com suporte à paginação e filtragem.

---
+++ src/repositories/categories-repository.ts
@@ -0,0 +1,41 @@
+import type { Category } from "~/database/schema";
+import type { Pagination } from "~/types/pagination";
+
+interface CreateCategoryParams {
+	name: string;
+	slug: string;
+}
+
+export interface FindAllCategoriesParams {
+	page: number;
+	limit: number;
+	filters: {
+		name?: string;
+		slug?: string;
+	};
+}
+
+export interface UpdateCategoryParams {
+	id: string;
+	data: {
+		name?: string;
+		slug?: string;
+	};
+}
+
+export interface CategoriesRespository {
+	create({ name, slug }: CreateCategoryParams): Promise<{ category: Category }>;
+
+	findById({ id }: { id: string }): Promise<Category | null>;
+
+	findBySlug({ slug }: { slug: string }): Promise<Category | null>;
+
+	findAll({ page, limit, filters }: FindAllCategoriesParams): Promise<{
+		categories: Category[];
+		pagination: Pagination;
+	}>;
+
+	update({ id, data }: UpdateCategoryParams): Promise<Category | null>;
+
+	delete({ id }: { id: string }): Promise<void>;
+}
---
+++ src/repositories/products-respository.ts
@@ -0,0 +1,74 @@
+import type { Product } from "~/database/schema";
+import type { Pagination } from "~/types/pagination";
+
+export interface CreateProductParams {
+	name: string;
+	description: string;
+	price: number;
+	discountPrice?: number;
+	stock: number;
+	colors: string[];
+	size?: string;
+	weight?: number;
+	dimensions?: Record<string, any>;
+	categoryId: string;
+	storeId: string;
+}
+
+export interface FindAllProductsParams {
+	page: number;
+	limit: number;
+	filters: {
+		name?: string;
+		description?: string;
+		price?: number;
+		discountPrice?: number;
+		stock?: number;
+		colors?: string[];
+		size?: string;
+		weight?: number;
+		dimensions?: Record<string, any>;
+		categoryId?: string;
+		storeId?: string;
+	};
+}
+
+export interface UpdateProductParams {
+	id: string;
+	data: {
+		name?: string;
+		description?: string;
+		price?: number;
+	};
+}
+
+export interface ProductsRespository {
+	create({
+		name,
+		description,
+		price,
+		discountPrice,
+		stock,
+		colors,
+		size,
+		weight,
+		dimensions,
+		categoryId,
+		storeId,
+	}: CreateProductParams): Promise<Product>;
+
+	findById({ id }: { id: string }): Promise<Product | null>;
+
+	findByStoreId({ storeId }: { storeId: string }): Promise<Product[]>;
+
+	findByCategoryId({ categoryId }: { categoryId: string }): Promise<Product[]>;
+
+	findAll({ page, limit, filters }: FindAllProductsParams): Promise<{
+		products: Product[];
+		pagination: Pagination;
+	}>;
+
+	update({ id, data }: UpdateProductParams): Promise<Product | null>;
+
+	delete({ id }: { id: string }): Promise<void>;
+}
