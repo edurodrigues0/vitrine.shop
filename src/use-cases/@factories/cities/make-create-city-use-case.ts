682cb3f7b1eb64b9629b0f401161cebdac227724
edurodrigues0 <edusacramento9@gmail.com>
2025-10-27 09:57:50 -0300

    feat: Criar caso de uso para a criação de cidades
    Adiciona a função makeCreateCityUseCase, que instancia o repositório DrizzleCitiesRepository e o caso de uso CreateCityUseCase. Essa implementação facilita a criação de cidades no sistema, integrando a lógica de negócios com o repositório de dados.

---
+++ src/use-cases/@factories/cities/make-create-city-use-case.ts
@@ -0,0 +1,10 @@
+import { DrizzleORM } from "~/database/connection";
+import { DrizzleCitiesRepository } from "~/repositories/drizzle/cities/cities-repository";
+import { CreateCityUseCase } from "~/use-cases/cities/create-city";
+
+export function makeCreateCityUseCase() {
+	const citiesRepository = new DrizzleCitiesRepository(DrizzleORM);
+	const useCase = new CreateCityUseCase(citiesRepository);
+
+	return useCase;
+}
